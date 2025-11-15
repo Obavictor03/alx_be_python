2d4fcfff25998fa2daba7613afad8df5cba29d7b
Obavictor03 <obarovictor03@gmail.com>
2025-11-15 12:50:00 +0100

    S.I calculator

--- python_introduction/simple_interest.py
+++ python_introduction/simple_interest.py
@@ -1,7 +1,7 @@
 #P-Principal, R-Rate, T-Time, I-Interest
-p = 1000
-r = 0.05
-t = 3
+principal = 1000
+rate = 0.05
+time = 3
 
-i = p * r * t
-print("The simple interest is:", i)
+interest = principal * rate * time
+print("The simple interest is:", interest)
