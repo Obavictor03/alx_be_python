b2a4fb630264b9bdbb1dce6b644283f8d280a230
Obavictor03 <obarovictor03@gmail.com>
2025-11-23 16:47:07 +0100

    coverting temperature

---
+++ fns_and_dsa/temp_conversion_tool.py
@@ -0,0 +1,21 @@
+FAHRENHEIT_TO_CELSIUS_FACTOR = 5/9
+CELSIUS_TO_FAHRENHEIT_FACTOR = 9/5
+
+def convert_to_celsius(fahrenheit):
+    return (fahrenheit - 32) * FAHRENHEIT_TO_CELSIUS_FACTOR
+
+def convert_to_fahrenheit(celsius):
+    return (celsius * CELSIUS_TO_FAHRENHEIT_FACTOR) + 32
+
+temp_value = int(input(("Enter the temperature to convert: ")))
+
+unit = input("Is this temperature in Celsius or Fahrenheit? (C/F): ").strip().upper()
+
+if unit == "C":
+    result = convert_to_fahrenheit(temp_value)
+    print(f"{temp_value}°C is equal to {result:.2f}°F")
+elif unit == "F":
+    result = convert_to_celsius(temp_value)
+    print(f"{temp_value}°F is equal to {result:.2f}°C")
+else:
+    print("Invalid unit")
