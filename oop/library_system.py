17ad15c39bd36a38b1597c6cc5f56127b6ae8f69
Obavictor03 <obarovictor03@gmail.com>
2025-12-07 17:02:17 +0100

    inheritance and composition

--- oop/library_system.py
+++ oop/library_system.py
@@ -20,7 +20,7 @@ class PrintBook(Book):
         self.page_count = page_count
     
     def __str__(self):
-        return f"{self.title} by {self.author}, Pages: {self.page_count}"
+        return f"{self.title} by {self.author}, Page Count: {self.page_count}"
 
 class Library:
     def __init__(self):
